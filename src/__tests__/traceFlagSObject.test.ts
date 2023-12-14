27c816aa9338500c6d1c99a1f175e3ae23e721b9
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-12-14 06:42:26 +0000

    feat: add initial test for trace flag sobject

---
+++ src/__tests__/traceFlagSObject.test.ts
@@ -0,0 +1,26 @@
+import {describe, expect, test} from '@jest/globals';
+import { TraceFlagSObjectBuilder } from '../traceFlagSObject';
+import { SalesforceLogLevel } from '../salesforceLogLevel';
+
+describe('trace flag sobject', () => {
+	it('should be able to create a trace flag sobject with all levels set', () => {
+
+		const builder = new TraceFlagSObjectBuilder();
+		builder.withApexCode(SalesforceLogLevel.debug);
+		builder.withCallout(SalesforceLogLevel.debug);
+		builder.withDatabase(SalesforceLogLevel.debug);
+		builder.withSystem(SalesforceLogLevel.debug);
+		builder.withValidation(SalesforceLogLevel.debug);
+		builder.withVisualforce(SalesforceLogLevel.debug);
+
+		const testObject = builder.build();
+
+		expect(testObject.apexCode).toBe(SalesforceLogLevel.debug);
+		expect(testObject.callout).toBe(SalesforceLogLevel.debug);
+		expect(testObject.database).toBe(SalesforceLogLevel.debug);
+		expect(testObject.system).toBe(SalesforceLogLevel.debug);
+		expect(testObject.validation).toBe(SalesforceLogLevel.debug);
+		expect(testObject.visualforce).toBe(SalesforceLogLevel.debug);
+	});
+});
+
