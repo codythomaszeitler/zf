5ad17b63bd0a212b967b835b79cf32172fe59f23
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-12-15 03:52:34 +0000

    feat: add debug level builder and sobject

--- src/debugLevelSObject.ts
+++ src/debugLevelSObject.ts
@@ -1,93 +1,92 @@
 import { SalesforceLogLevel } from "./salesforceLogLevel";
 
 export interface DebugLevel {
-    id? : string;
-    developerName: string;
-    language: SalesforceLogLevel;
-    apexCode: SalesforceLogLevel;
-    visualforce: SalesforceLogLevel;
-    system: SalesforceLogLevel;
-    database: SalesforceLogLevel;
-    callout: SalesforceLogLevel;
-    validation: SalesforceLogLevel;
-    workflow: SalesforceLogLevel;
+	id?: string;
+	developerName: string;
+	language: SalesforceLogLevel;
+	apexCode: SalesforceLogLevel;
+	visualforce: SalesforceLogLevel;
+	system: SalesforceLogLevel;
+	database: SalesforceLogLevel;
+	callout: SalesforceLogLevel;
+	validation: SalesforceLogLevel;
+	workflow: SalesforceLogLevel;
 }
 
 export class DebugLevelBuilder {
-    private id?: string;
-    private developerName?: string;
-    private language?: SalesforceLogLevel;
-    private apexCode?: SalesforceLogLevel;
-    private visualforce?: SalesforceLogLevel;
-    private system?: SalesforceLogLevel;
-    private database?: SalesforceLogLevel;
-    private callout?: SalesforceLogLevel;
-    private validation?: SalesforceLogLevel;
-    private workflow?: SalesforceLogLevel;
+	private id?: string;
+	private developerName?: string;
+	private language?: SalesforceLogLevel;
+	private apexCode?: SalesforceLogLevel;
+	private visualforce?: SalesforceLogLevel;
+	private system?: SalesforceLogLevel;
+	private database?: SalesforceLogLevel;
+	private callout?: SalesforceLogLevel;
+	private validation?: SalesforceLogLevel;
+	private workflow?: SalesforceLogLevel;
 
-    public withId(id: string): DebugLevelBuilder {
-        this.id = id;
-        return this;
-    }
+	public withId(id: string): DebugLevelBuilder {
+		this.id = id;
+		return this;
+	}
 
-    public withDeveloperName(developerName: string): DebugLevelBuilder {
-        this.developerName = developerName;
-        return this;
-    }
+	public withDeveloperName(developerName: string): DebugLevelBuilder {
+		this.developerName = developerName;
+		return this;
+	}
 
-    public withLanguage(language: SalesforceLogLevel): DebugLevelBuilder {
-        this.language = language;
-        return this;
-    }
+	public withLanguage(language: SalesforceLogLevel): DebugLevelBuilder {
+		this.language = language;
+		return this;
+	}
 
-    public withApexCode(apexCode: SalesforceLogLevel): DebugLevelBuilder {
-        this.apexCode = apexCode;
-        return this;
-    }
+	public withApexCode(apexCode: SalesforceLogLevel): DebugLevelBuilder {
+		this.apexCode = apexCode;
+		return this;
+	}
 
-    public withVisualforce(visualforce: SalesforceLogLevel): DebugLevelBuilder {
-        this.visualforce = visualforce;
-        return this;
-    }
+	public withVisualforce(visualforce: SalesforceLogLevel): DebugLevelBuilder {
+		this.visualforce = visualforce;
+		return this;
+	}
 
-    public withSystem(system: SalesforceLogLevel): DebugLevelBuilder {
-        this.system = system;
-        return this;
-    }
+	public withSystem(system: SalesforceLogLevel): DebugLevelBuilder {
+		this.system = system;
+		return this;
+	}
 
-    public withDatabase(database: SalesforceLogLevel): DebugLevelBuilder {
-        this.database = database;
-        return this;
-    }
+	public withDatabase(database: SalesforceLogLevel): DebugLevelBuilder {
+		this.database = database;
+		return this;
+	}
 
-    public withCallout(callout: SalesforceLogLevel): DebugLevelBuilder {
-        this.callout = callout;
-        return this;
-    }
+	public withCallout(callout: SalesforceLogLevel): DebugLevelBuilder {
+		this.callout = callout;
+		return this;
+	}
 
-    public withValidation(validation: SalesforceLogLevel): DebugLevelBuilder {
-        this.validation = validation;
-        return this;
-    }
+	public withValidation(validation: SalesforceLogLevel): DebugLevelBuilder {
+		this.validation = validation;
+		return this;
+	}
 
-    public withWorkflow(workflow: SalesforceLogLevel): DebugLevelBuilder {
-        this.workflow = workflow;
-        return this;
-    }
+	public withWorkflow(workflow: SalesforceLogLevel): DebugLevelBuilder {
+		this.workflow = workflow;
+		return this;
+	}
 
-    public build(): DebugLevel {
-        return {
-            id: this.id,
-            developerName: this.developerName,
-            masterLabel: this.masterLabel,
-            language: this.language,
-            apexCode: this.apexCode,
-            visualforce: this.visualforce,
-            system: this.system,
-            database: this.database,
-            callout: this.callout,
-            validation: this.validation,
-            workflow: this.workflow
-        };
-    }
+	public build(): DebugLevel {
+		return {
+			id: this.id,
+			developerName: this.developerName || "",
+			language: this.language || SalesforceLogLevel.none,
+			apexCode: this.apexCode || SalesforceLogLevel.none,
+			visualforce: this.visualforce || SalesforceLogLevel.none,
+			system: this.system || SalesforceLogLevel.none,
+			database: this.database || SalesforceLogLevel.none,
+			callout: this.callout || SalesforceLogLevel.none,
+			validation: this.validation || SalesforceLogLevel.none,
+			workflow: this.workflow || SalesforceLogLevel.none
+		};
+	}
 }
