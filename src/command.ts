4ed3f1b6a922d02a0f295e23a3b125f2aac750c1
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-12-17 19:38:19 +0000

    feat: change debug trace flag creation to object structure

---
+++ src/command.ts
@@ -0,0 +1,26 @@
+import { IntegratedDevelopmentEnvironment } from "./integratedDevelopmentEnvironment";
+import { SalesforceCli } from "./salesforceCli";
+
+export abstract class Command {
+
+	private readonly ide: IntegratedDevelopmentEnvironment;
+	private readonly cli: SalesforceCli;
+
+	protected constructor(params: CommandParams) {
+		this.ide = params.ide;
+		this.cli = params.cli;
+	}
+
+	protected getIde() {
+		return this.ide;
+	}
+
+	protected getCli() {
+		return this.cli;
+	}
+}
+
+export interface CommandParams {
+	ide: IntegratedDevelopmentEnvironment,
+	cli: SalesforceCli
+}
--- src/genDebugTraceFlag.ts
+++ src/genDebugTraceFlag.ts
@@ -1,7 +1,9 @@
+import { Command, CommandParams } from "./command";
 import { addHours } from "./dateUtils";
 import { DebugLevel, DebugLevelBuilder } from "./debugLevelSObject";
 import { DebugLogLevelSelector } from "./debugLogLevelSelector";
 import { getCurrentUser } from "./getCurrentUser";
+import { IntegratedDevelopmentEnvironment } from "./integratedDevelopmentEnvironment";
 import { SalesforceCli } from "./salesforceCli";
 import { SalesforceLogLevel } from "./salesforceLogLevel";
 import { SalesforceOrg } from "./salesforceOrg";
@@ -10,64 +12,134 @@ import { LogType, TraceFlagSObjectBuilder } from "./traceFlagSObject";
 export async function generateDebugTraceFlag(params: {
 	targetOrg: SalesforceOrg,
 	salesforceCli: SalesforceCli,
-	debugLogLevelApiName: string
+	debugLogLevelApiName: string,
+	ide: IntegratedDevelopmentEnvironment
 }) {
-	const currentUser = await getCurrentUser({
-		targetOrg: params.targetOrg,
-		cli: params.salesforceCli
-	});
-	if (!currentUser) {
-		throw new Error(`Could not find user for org ${params.targetOrg.getAlias()}`);
-	}
-
-	const debugLogLevel = await generateDebugLogLevelOrGetExisting({
-		targetOrg: params.targetOrg,
+	const debugTraceFlagGenerateCommand = new DebugTraceFlagGenerateCommand({
+		ide: params.ide,
 		cli: params.salesforceCli,
-		debugLogLevelApiName: params.debugLogLevelApiName
+		getOrCreateCommand: new DebugLogLevelGetOrCreateCommand({
+			ide: params.ide,
+			cli: params.salesforceCli
+		})
 	});
 
-	const traceFlagSObjectBuilder = new TraceFlagSObjectBuilder();
-	traceFlagSObjectBuilder.withDebugLevelId(debugLogLevel.id);
-	traceFlagSObjectBuilder.withLogType(LogType.developerLog);
-	traceFlagSObjectBuilder.withTracedEntityId(currentUser?.userId);
-
-	const expirationDate = addHours(new Date(Date.now()), 23);
-	traceFlagSObjectBuilder.withExpirationDate(expirationDate);
-
-	await params.salesforceCli.dataCreateRecord({
+	return debugTraceFlagGenerateCommand.execute({
 		targetOrg: params.targetOrg,
-		sObject: traceFlagSObjectBuilder.build()
+		developerName: params.debugLogLevelApiName
 	});
 }
 
 export async function generateDebugLogLevelOrGetExisting(params: {
 	targetOrg: SalesforceOrg,
 	cli: SalesforceCli,
-	debugLogLevelApiName: string
+	debugLogLevelApiName: string,
+	ide: IntegratedDevelopmentEnvironment
 }): Promise<DebugLevel> {
-	const debugLogLevelSelector = new DebugLogLevelSelector({
-		cli: params.cli
+	const getOrCreate = new DebugLogLevelGetOrCreateCommand({
+		cli: params.cli,
+		ide: params.ide
 	});
 
-	const debugLevel = await debugLogLevelSelector.queryByDeveloperName({
+	return getOrCreate.execute({
 		targetOrg: params.targetOrg,
 		developerName: params.debugLogLevelApiName
 	});
+}
 
-	if (debugLevel) {
-		return debugLevel;
+export class DebugTraceFlagGenerateCommand extends Command {
+
+	private readonly getOrCreateCommand: DebugLogLevelGetOrCreateCommand;
+
+	public constructor(params: {
+		ide: IntegratedDevelopmentEnvironment,
+		cli: SalesforceCli,
+		getOrCreateCommand: DebugLogLevelGetOrCreateCommand
+	}) {
+		super(params);
+		this.getOrCreateCommand = params.getOrCreateCommand;
 	}
 
-	const debugLogLevelBuilder = new DebugLevelBuilder({
-		developerName: params.debugLogLevelApiName
-	});
-	debugLogLevelBuilder.withApexCode(SalesforceLogLevel.debug);
+	public async execute(params: {
+		targetOrg: SalesforceOrg,
+		developerName: string
+	}) {
+		const currentUser = await getCurrentUser({
+			targetOrg: params.targetOrg,
+			cli: this.getCli()
+		});
+		if (!currentUser) {
+			throw new Error(`Could not find user for org ${params.targetOrg.getAlias()}`);
+		}
 
-	const newDebugLogLevel = debugLogLevelBuilder.build();
-	await params.cli.dataCreateRecord({
-		targetOrg: params.targetOrg,
-		sObject: newDebugLogLevel
-	});
+		const debugLogLevel = await this.getOrCreateCommand.execute({
+			targetOrg: params.targetOrg,
+			developerName: params.developerName
+		});
 
-	return newDebugLogLevel;
+		const traceFlagSObjectBuilder = new TraceFlagSObjectBuilder();
+		traceFlagSObjectBuilder.withDebugLevelId(debugLogLevel.id);
+		traceFlagSObjectBuilder.withLogType(LogType.developerLog);
+		traceFlagSObjectBuilder.withTracedEntityId(currentUser?.userId);
+
+		const expirationDate = addHours(new Date(Date.now()), 23);
+		traceFlagSObjectBuilder.withExpirationDate(expirationDate);
+
+		await this.getCli().dataCreateRecord({
+			targetOrg: params.targetOrg,
+			sObject: traceFlagSObjectBuilder.build()
+		});
+	}
+}
+
+export class DebugLogLevelGetOrCreateCommand extends Command {
+
+	public constructor(params: CommandParams) {
+		super(params);
+	}
+
+	public async execute(params: {
+		targetOrg: SalesforceOrg,
+		developerName: string
+	}): Promise<DebugLevel> {
+		const debugLevel = await this.queryByDeveloperName({
+			targetOrg: params.targetOrg,
+			developerName: params.developerName
+		});
+
+		if (debugLevel) {
+			return debugLevel;
+		}
+
+		const debugLogLevelBuilder = new DebugLevelBuilder({
+			developerName: params.developerName
+		});
+		debugLogLevelBuilder.withApexCode(SalesforceLogLevel.debug);
+
+		const newDebugLogLevel = debugLogLevelBuilder.build();
+		await this.getCli().dataCreateRecord({
+			targetOrg: params.targetOrg,
+			sObject: newDebugLogLevel
+		});
+
+		return newDebugLogLevel;
+	}
+
+	protected async queryByDeveloperName(params: {
+		targetOrg: SalesforceOrg,
+		developerName: string
+	}) {
+		const debugLogLevelSelector = new DebugLogLevelSelector({
+			cli: this.getCli()
+		});
+
+		const debugLevel = await debugLogLevelSelector.queryByDeveloperName({
+			targetOrg: params.targetOrg,
+			developerName: params.developerName
+		});
+
+		if (debugLevel) {
+			return debugLevel;
+		}
+	}
 }
