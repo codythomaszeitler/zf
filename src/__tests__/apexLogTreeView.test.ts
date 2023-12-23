6be06096d7ed0dfc3b4f3ca53d2b63fbf34fd5d9
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-12-22 21:16:35 -0500

    test: add apex tree log view test

---
+++ src/__tests__/apexLogTreeView.test.ts
@@ -0,0 +1,140 @@
+import { ApexLog } from "../apexLog";
+import { APEX_LOG_TREE_API_NAME, ApexLogTreeView } from "../apexLogTreeView";
+import { SalesforceOrg } from "../salesforceOrg";
+import { MockFileSystem } from "./__mocks__/mockFileSystem";
+import { MockIDE } from "./__mocks__/mockIntegratedDevelopmentEnvironment";
+import { MockSalesforceCli } from "./__mocks__/mockSalesforceCli";
+import { describe, expect, test } from '@jest/globals';
+import { genRandomId } from "./salesforceId.test";
+import { getLogFileUri } from "../showApexLogCommand";
+
+describe('apex log tree view', () => {
+
+	let cli: MockSalesforceCli;
+	let ide: MockIDE;
+	let fs: MockFileSystem;
+	let logDir: string;
+	let org: SalesforceOrg;
+
+	beforeEach(() => {
+		fs = new MockFileSystem();
+
+		cli = new MockSalesforceCli({
+			filesystem: fs
+		});
+
+		ide = new MockIDE({
+			filesystem: fs
+		});
+
+		logDir = 'testLogDir';
+
+		org = new SalesforceOrg({
+			alias: 'cso',
+			isActive: true
+		});
+		cli.setDefaultOrg(org);
+	});
+
+	function createApexLog() {
+		const randomId = genRandomId('ApexLog');
+		const startTime = new Date(Date.parse("2023-12-20T07:04:17+0000"));
+		const apexLog = new ApexLog({
+			id: randomId,
+			application: 'Unknown',
+			duration: 89,
+			logLength: 2680,
+			operation: 'Api',
+			startTime: startTime,
+			status: 'Success'
+		});
+		return apexLog;
+	}
+
+	it('should have the correct api name when commanded to get it', () => {
+		const testObject = new ApexLogTreeView({
+			cli,
+			ide,
+			logDir
+		});
+		expect(testObject.uniqueName).toBe(APEX_LOG_TREE_API_NAME);
+	});
+
+	it('should create a two layer tree, first layer is org name, second layer are logs', async () => {
+		const apexLog = createApexLog();
+
+		cli.addApexLog({
+			targetOrg: org,
+			apexLog
+		});
+
+		const testObject = new ApexLogTreeView({
+			cli,
+			ide,
+			logDir
+		});
+
+		const rootNode = await testObject.getRootNode({
+			targetOrg: org
+		});
+
+		expect(rootNode.label).toBe(org.getAlias());
+		expect(rootNode.value).toBeUndefined();
+		expect(rootNode.children.length).toBe(1);
+
+		const child = rootNode.children[0];
+		expect(child.label).toBe(apexLog.getTreeViewString());
+		expect(child.value).toBe(apexLog);
+	});
+
+	it('should be able to generate file if cannot find file in filesystem', async () => {
+		const testObject = new ApexLogTreeView({
+			cli,
+			ide,
+			logDir
+		});
+
+		const apexLog = createApexLog();
+		cli.addApexLog({
+			targetOrg: org,
+			apexLog
+		});
+
+		await testObject.onSelect({
+			value: apexLog
+		});
+
+		const uri = getLogFileUri({
+			targetOrg: org,
+			logDir: logDir,
+			logId: apexLog.getId()
+		});
+		expect(ide.toHaveShownTextDocument(uri)).toBeTruthy();
+	});
+
+	it('should be able reuse file if file in filesystem', async () => {
+		const testObject = new ApexLogTreeView({
+			cli,
+			ide,
+			logDir
+		});
+
+		cli.toThrowOnApexGetLog = new Error('Should not have called apex get log!');
+		const apexLog = createApexLog();
+
+		const uri = getLogFileUri({
+			targetOrg: org,
+			logDir: logDir,
+			logId: apexLog.getId()
+		});
+		fs.create({
+			uri
+		});
+
+		await testObject.onSelect({
+			value: apexLog
+		});
+
+		expect(ide.toHaveShownTextDocument(uri)).toBeTruthy();
+	});
+});
