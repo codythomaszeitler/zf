9616e91003fbcfd30189d2a528fade5ed02fa985
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-12-25 10:43:01 -0500

    feat: add apex test queue item selector

--- src/__tests__/apexTestQueueItemSelector.test.ts
+++ src/__tests__/apexTestQueueItemSelector.test.ts
@@ -3,6 +3,10 @@ import { SalesforceOrg } from "../salesforceOrg";
 import { MockFileSystem } from "./__mocks__/mockFileSystem";
 import { MockIDE } from "./__mocks__/mockIntegratedDevelopmentEnvironment";
 import { MockSalesforceCli } from "./__mocks__/mockSalesforceCli";
+import { APEX_TEST_QUEUE_ITEM_SOBJECT_NAME, ApexTestQueueItemBuilder, ApexTestQueueStatus } from '../apexTestQueueItem';
+import { genRandomId } from './salesforceId.test';
+import { SalesforceId } from '../salesforceId';
+import { ApexTestQueueItemSelector } from '../apexTestQueueItemSelector';
 
 describe('apex test queue item selector', () => {
 
@@ -11,6 +15,8 @@ describe('apex test queue item selector', () => {
 	let ide: MockIDE;
 
 	let org: SalesforceOrg;
+
+	let testObject: ApexTestQueueItemSelector;
 
 	beforeEach(() => {
 		org = new SalesforceOrg({
@@ -27,9 +33,51 @@ describe('apex test queue item selector', () => {
 		});
 
 		cli.setDefaultOrg(org);
+
+		testObject = new ApexTestQueueItemSelector({
+			cli
+		});
 	});
 
-	it('should be able to convert generic sobjects from query into ApexTestQueueItems', () => {
-		// There should be an ApexTestQueueItem at this point?
+	async function genApexQueueItem(params: {
+		status: ApexTestQueueStatus,
+		parentJobId: SalesforceId
+	}) {
+		const randomId = genRandomId(APEX_TEST_QUEUE_ITEM_SOBJECT_NAME);
+		const builder = new ApexTestQueueItemBuilder({
+			id: randomId
+		});
+		const apexTestQueueItem = builder.withStatus(params.status).withParentJobId(params.parentJobId).build();
+
+		await cli.dataUpsertRecord({
+			targetOrg: org,
+			sObject: apexTestQueueItem
+		});
+	}
+
+	it('should be able to convert generic sobjects from query into ApexTestQueueItems', async () => {
+		const parentJobId = genRandomId('AsyncApexJob');
+		await genApexQueueItem({
+			status: "Queued",
+			parentJobId
+		});
+
+		const apexTestQueueItems = await testObject.queryByParentJobId({
+			parentJobId,
+			targetOrg: org
+		});
+		expect(apexTestQueueItems).toHaveLength(1);
+		const apexTestQueueItem = apexTestQueueItems[0];
+
+		expect(apexTestQueueItem.parentJobId).toBe(parentJobId);
+	});
+
+	it('should be able to convert when no sobjects are found', async () => {
+		const parentJobId = genRandomId('AsyncApexJob');
+		const apexTestQueueItems = await testObject.queryByParentJobId({
+			parentJobId,
+			targetOrg: org
+		});
+		expect(apexTestQueueItems).toHaveLength(0);
 	});
 });
---
+++ src/apexTestQueueItemSelector.ts
@@ -0,0 +1,29 @@
+import { APEX_TEST_QUEUE_ITEM_SOBJECT_NAME, ApexTestQueueItem, ApexTestQueueItemBuilder, ApexTestQueueStatus } from "./apexTestQueueItem";
+import { SalesforceId } from "./salesforceId";
+import { SalesforceOrg } from "./salesforceOrg";
+import { Selector } from "./selector";
+
+export class ApexTestQueueItemSelector extends Selector {
+
+	public async queryByParentJobId(params: { targetOrg: SalesforceOrg; parentJobId: SalesforceId; }) {
+		const dataQueryResult = await this.getCli().dataQuery({
+			query: {
+				from: APEX_TEST_QUEUE_ITEM_SOBJECT_NAME,
+				fields: ['Status', 'ParentJobId'],
+				where: `ParentJobId = '${params.parentJobId}'`
+			},
+			targetOrg: params.targetOrg
+		});
+		const sObjects = dataQueryResult.getSObjects();
+		const apexTestQueueItems: ApexTestQueueItem[] = sObjects.map(sObject => {
+			const builder = new ApexTestQueueItemBuilder({
+				id: SalesforceId.get(sObject["Id"])
+			});
+			const status = sObject["Status"] as ApexTestQueueStatus;
+			const parentJobId = SalesforceId.get(sObject["ParentJobId"]);
+			return builder.withStatus(status).withParentJobId(parentJobId).build();
+		});
+
+		return apexTestQueueItems;
+	}
+}
