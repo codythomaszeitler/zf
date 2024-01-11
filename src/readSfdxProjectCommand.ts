5326e04cec8a5acfd1862535a67f35520d652327
Cody T. Zeitler <codyzeitler12@gmail.com>
2024-01-11 00:25:40 -0700

    refactor: move cache on save calls into one spot

--- src/readSfdxProjectCommand.ts
+++ src/readSfdxProjectCommand.ts
@@ -1,10 +1,32 @@
-import { Command } from "./command";
-import { Uri } from "./integratedDevelopmentEnvironment";
+import { Command, CommandParams } from "./command";
+import { TextDocument, Uri } from "./integratedDevelopmentEnvironment";
 
 export function getSfdxProjectUri(params: {
 	currentDir: Uri
 }) {
 	return Uri.join(params.currentDir, "sfdx-project.json");
+}
+
+export function genCacheSfdxProjectOnSave({
+	cli,
+	ide
+}: CommandParams) {
+	function runCacheReadSfdxProjectCommand() {
+		const readSfdxProjectCommand = new CacheReadSfdxProjectCommand({
+			cli,
+			ide
+		});
+		readSfdxProjectCommand.execute();
+	}
+
+	runCacheReadSfdxProjectCommand();
+
+	return async function ({ textDocuments }: { textDocuments: TextDocument[] }) {
+		const sfdxProjectUri = textDocuments.find(textDocument => textDocument.uri.getBaseName() === 'sfdx-project.json');
+		if (sfdxProjectUri) {
+			runCacheReadSfdxProjectCommand();
+		}
+	};
 }
 
 export class ReadSfdxProjectCommand extends Command {
