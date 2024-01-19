e7d5a4ccdc68c6cd5a4e8fb89ed0abfe36c22852
Cody T. Zeitler <codyzeitler12@gmail.com>
2024-01-19 07:25:37 +0000

    feat: add apex parser

--- src/apexParser.ts
+++ src/apexParser.ts
@@ -1,5 +1,5 @@
 import { AbstractParseTreeVisitor } from "antlr4ts/tree/AbstractParseTreeVisitor";
-import * as _ApexParser from "apex-parser";
+import * as _ApexParser from 'apex-parser';
 
 export class ApexParser {
 	public parse(input: string): ApexClassSymbolTable {
@@ -12,41 +12,141 @@ export class ApexParser {
 		const visitor = new ZfApexParserVisitor();
 		visitor.visit(context);
 
-		return new ApexClassSymbolTable();
+		return new ApexClassSymbolTable({
+			methods: visitor.members.filter(member => member.apexMemberType === 'method').map(member => {
+				const apexMethod: ApexMethod = {
+					isPublic: member.isPublic,
+					name: member.methodName,
+					returnType: member.returnType
+				};
+				return apexMethod;
+			}),
+			properties: visitor.members.filter(member => member.apexMemberType === 'field').map(member => {
+				const apexProperty: ApexProperty = {
+					isPublic: member.isPublic,
+					name: member.methodName,
+					type: member.returnType
+				};
+				return apexProperty;
+			}),
+			className: visitor.className
+		});
 	}
 }
+
+export type ApexMethod = {
+	returnType: string;
+	name: string;
+	isPublic: boolean;
+};
+
+export type ApexProperty = {
+	type: string;
+	name: string;
+	isPublic: boolean;
+};
 
 export class ApexClassSymbolTable {
-	public getPublicMethods() {
 
+	private readonly methods: ApexMethod[];
+	private readonly properties: ApexProperty[];
+	private readonly className: string;
+
+	public constructor({
+		className,
+		methods,
+		properties
+	}: {
+		methods: ApexMethod[],
+		properties: ApexProperty[],
+		className: string
+	}) {
+		this.methods = methods;
+		this.properties = properties;
+		this.className = className;
+	}
+
+	public getPublicMethods() {
+		return this.methods.filter(method => method.isPublic);
+	}
+
+	public getPublicFields() {
+		return this.properties.filter(property => property.isPublic);
+	}
+
+	public getClassName() {
+		return this.className;
 	}
 }
 
-class ZfApexParserVisitor extends AbstractParseTreeVisitor<number> implements _ApexParser.ApexParserVisitor<number> {
-	public methodCounter: number = 0;
+type ApexMemberType = 'method' | 'field';
 
-	visitTypeRef(ctx: _ApexParser.TypeRefContext): number {
-		console.log(ctx);
-		return 0;
+type VisitorResult = {
+	isPublic: boolean,
+	returnType: string,
+	methodName: string
+	apexMemberType: ApexMemberType;
+};
+
+class ZfApexParserVisitor extends AbstractParseTreeVisitor<void> implements _ApexParser.ApexParserVisitor<void> {
+
+	members: VisitorResult[] = [];
+	className: string = "";
+
+	visitClassDeclaration(ctx: _ApexParser.ClassDeclarationContext): void {
+		this.className = ctx.id().text;
+		super.visitChildren(ctx);
 	}
 
-	visitTypeDeclaration(ctx: _ApexParser.TypeDeclarationContext): number {
-		console.log(ctx);
-		return 0;
+	visitClassBodyDeclaration(ctx: _ApexParser.ClassBodyDeclarationContext): void {
+		const isPublicMember = () => {
+			return ctx.modifier().some(value => {
+				const tokens = value.getTokens(_ApexParser.ApexLexer.PUBLIC);
+				return !!tokens.length;
+			});
+		};
+
+		const getVisitorResults = () => {
+			const memberDecl = ctx.memberDeclaration();
+			if (ctx.modifier() && memberDecl) {
+				const methodDecl = memberDecl.methodDeclaration();
+				if (methodDecl) {
+					const result: VisitorResult = {
+						isPublic: isPublicMember(),
+						returnType: methodDecl.typeRef()?.text ?? "",
+						methodName: methodDecl.id().text ?? "",
+						apexMemberType: 'method'
+					};
+					return [result];
+				}
+
+				const fieldDecl = memberDecl.fieldDeclaration();
+				if (fieldDecl) {
+					const isPublicField = (fieldDecl: _ApexParser.VariableDeclaratorContext) => {
+						return !!fieldDecl.getTokens(_ApexParser.ApexLexer.PUBLIC);
+					};
+
+					const varDecls = fieldDecl.variableDeclarators().variableDeclarator();
+					return varDecls.map(varDecl => {
+						const result: VisitorResult = {
+							isPublic: isPublicField(varDecl),
+							returnType: fieldDecl.typeRef().text,
+							apexMemberType: "field",
+							methodName: varDecl.id().text
+						};
+						return result;
+					});
+				}
+			}
+
+			return [];
+		};
+
+		const visitorResults = getVisitorResults();
+		this.members.push(...visitorResults);
 	}
 
-	visitTypeRefPrimary(ctx: _ApexParser.TypeRefPrimaryContext) : number {
-		console.log(ctx);
-		return 0;
-	}
-
-	visitMethodDeclaration(ctx: _ApexParser.MethodDeclarationContext): number {
-		console.log(ctx);
-		this.methodCounter += 1;
-		return 1 + super.visitChildren(ctx);
-	}
-
-	protected defaultResult(): number {
-		return 0;
+	protected defaultResult(): void {
+		return undefined;
 	}
 }
