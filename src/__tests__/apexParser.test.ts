8e9b791e30d09efa5cbe3bee9d50ad1af56e8f78
Cody T. Zeitler <codyzeitler12@gmail.com>
2024-01-28 03:56:17 +0000

    feat: add apex parser for test methods

--- src/__tests__/apexParser.test.ts
+++ src/__tests__/apexParser.test.ts
@@ -1,88 +1,47 @@
 import { describe, expect } from '@jest/globals';
 import { ApexParser } from '../apexParser';
 describe('apex parser', () => {
-	it('should be able to get all public methods on an apex class', () => {
+	it('should be able to get all public test methods on an apex class', () => {
 		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { public Integer foo() {return 3;} } ');
+		const apexClass = testObject.parse('@IsTest public class TestClass { @IsTest public static void testMethod1() {} } ');
 
 		expect(apexClass.getClassName()).toBe('TestClass');
+		expect(apexClass.getIsTestClass()).toBe(true);
 
-		const apexMethods = apexClass.getPublicMethods();
+		const apexMethods = apexClass.getTestMethods();
 
 		expect(apexMethods).toHaveLength(1);
 
-		const publicFooMethod = apexMethods[0];
-		expect(publicFooMethod.name).toBe('foo');
-		expect(publicFooMethod.returnType).toBe('Integer');
+		const publicTestMethod1 = apexMethods[0];
+		expect(publicTestMethod1.name).toBe('testMethod1');
 	});
 
-	it('should be able to get all public methods on an apex class with same case sensitivity', () => {
+	it('should be able to get all private test methods on an apex class', () => {
 		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { Public Integer foo() {return 3;} } ');
+		const apexClass = testObject.parse('@IsTest public class TestClass { @IsTest static void testMethod1() {} } ');
 
-		const apexMethods = apexClass.getPublicMethods();
+		expect(apexClass.getClassName()).toBe('TestClass');
+		expect(apexClass.getIsTestClass()).toBe(true);
+
+		const apexMethods = apexClass.getTestMethods();
 
 		expect(apexMethods).toHaveLength(1);
 
-		const publicFooMethod = apexMethods[0];
-		expect(publicFooMethod.name).toBe('foo');
-		expect(publicFooMethod.returnType).toBe('Integer');
+		const publicTestMethod1 = apexMethods[0];
+		expect(publicTestMethod1.name).toBe('testMethod1');
 	});
 
-	it('should be able to get all public fields on an apex class', () => {
+	it('should be get no test methods if there are non in class', () => {
 		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { public Integer a; } ');
+		const apexClass = testObject.parse('@IsTest public class TestClass { static void testMethod1() {} } ');
 
 		expect(apexClass.getClassName()).toBe('TestClass');
+		// This is NOT test class since there are no test methods. 
+		// You can have "Test utils" that are not runnable test classes, yet they have the @IsTest modifier
+		expect(apexClass.getIsTestClass()).toBe(false); 
 
-		const apexFields = apexClass.getPublicFields();
+		const apexMethods = apexClass.getTestMethods();
 
-		expect(apexFields).toHaveLength(1);
-
-		const publicAProperty = apexFields[0];
-		expect(publicAProperty.name).toBe('a');
-		expect(publicAProperty.type).toBe('Integer');
-	});
-
-	it('should be able to get all public fields on an apex class when there is a assignment expression', () => {
-		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { public Integer a = 5; } ');
-
-		expect(apexClass.getClassName()).toBe('TestClass');
-
-		const apexFields = apexClass.getPublicFields();
-
-		expect(apexFields).toHaveLength(1);
-
-		const publicAProperty = apexFields[0];
-		expect(publicAProperty.name).toBe('a');
-		expect(publicAProperty.type).toBe('Integer');
-	});
-
-	it('should be able to get all public fields on an apex class when one field has multiple decls', () => {
-		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { public Integer a,b,c; } ');
-
-		expect(apexClass.getClassName()).toBe('TestClass');
-
-		const apexFields = apexClass.getPublicFields();
-		expect(apexFields).toHaveLength(3);
-
-		const names = apexFields.map(apexField => apexField.name);
-		expect(names).toEqual(['a', 'b', 'c']);
-	});
-
-	it('should be able to get all public fields on an apex class when one field has a generic list return', () => {
-		const testObject = new ApexParser();
-		const apexClass = testObject.parse('public class TestClass { public List<Integer> foo() {return new List<Integer>(); } } ');
-
-		expect(apexClass.getClassName()).toBe('TestClass');
-
-		const apexMethods = apexClass.getPublicMethods();
-
-		expect(apexMethods).toHaveLength(1);
-
-		const apexMethod = apexMethods[0];
-		expect(apexMethod.returnType).toBe('List<Integer>');
+		expect(apexMethods).toHaveLength(0);
 	});
 });
