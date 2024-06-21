4f6989afec815b8b013903baa2fbc798a66e7f11
Cody T. Zeitler <codyzeitler12@gmail.com>
2024-06-21 00:39:47 -0700

    fix: ensure that default org users correct username comparison against sandbox

--- src/quickDefaultOrgSalesforceCli.ts
+++ src/quickDefaultOrgSalesforceCli.ts
@@ -1,8 +1,9 @@
 import { Executor } from "./executor";
-import { IntegratedDevelopmentEnvironment } from "./integratedDevelopmentEnvironment";
+import { IntegratedDevelopmentEnvironment, Uri } from "./integratedDevelopmentEnvironment";
 import { Logger } from "./logger";
 import { SalesforceOrg } from "./salesforceOrg";
 import { SfSalesforceCli } from "./sfSalesforceCli";
+import { homedir } from 'os';
 
 import { z } from "zod";
 
@@ -11,7 +12,13 @@ const salesforceConfigSchema = z.object({
 	[targetOrgKey]: z.string().readonly()
 });
 
+
 export type SalesforceSfConfig = z.infer<typeof salesforceConfigSchema>;
+
+const salesforceAliasSchema = z.object({
+	orgs: z.record(z.string().readonly(), z.string().readonly())
+});
+export type SalesforceSfAlias = z.infer<typeof salesforceAliasSchema>;
 
 export class QuickDefaultOrgSfSalesforceCli extends SfSalesforceCli {
 	private readonly ide: IntegratedDevelopmentEnvironment;
@@ -25,8 +32,13 @@ export class QuickDefaultOrgSfSalesforceCli extends SfSalesforceCli {
 	public async getDefaultOrg(): Promise<SalesforceOrg | null> {
 		try {
 			const config = await this.readSfConfig();
-			if (!this.cachedDefaultOrg || config[targetOrgKey] !== this.cachedDefaultOrg.getAlias()) {
+			if (!this.cachedDefaultOrg) {
 				this.cachedDefaultOrg = await super.getDefaultOrg();
+			} else {
+				const alias = await this.getAliasFor(this.cachedDefaultOrg);
+				if (config[targetOrgKey] !== alias) {
+					this.cachedDefaultOrg = await super.getDefaultOrg();
+				}
 			}
 			return this.cachedDefaultOrg;
 		} catch (e: unknown) {
@@ -34,6 +46,39 @@ export class QuickDefaultOrgSfSalesforceCli extends SfSalesforceCli {
 				Logger.get().error(e);
 			}
 			return super.getDefaultOrg();
+		}
+	}
+
+	private async getAliasFor(targetOrg: SalesforceOrg) {
+		if (targetOrg.getIsScratchOrg()) {
+			return targetOrg.getAlias();
+		}
+
+		const alias = await this.readAliasFile();
+		if (alias.orgs) {
+			let found = undefined;
+			Object.entries(alias.orgs).forEach(([alias, username]) => {
+				if (username === targetOrg.getAlias()) {
+					found = alias;
+				}
+			});
+			return found;
+		} else {
+			return undefined;
+		}
+	}
+
+	private async readAliasFile(): Promise<SalesforceSfAlias> {
+		try {
+			const uri = getDefaultSfAliasUri();
+			const contents = await this.ide.readFile({ uri });
+			const alias = JSON.parse(contents);
+			return salesforceAliasSchema.parse(alias);
+		} catch (e) {
+			if (e instanceof Error) {
+				Logger.get().error(e);
+			}
+			return {};
 		}
 	}
 
@@ -47,4 +92,12 @@ export class QuickDefaultOrgSfSalesforceCli extends SfSalesforceCli {
 
 export function getDefaultSfConfigUri(ide: IntegratedDevelopmentEnvironment) {
 	return ide.generateUri('.sf', 'config.json');
+}
+
+export function getDefaultSfAliasUri() {
+	const homedirUri = Uri.from({
+		scheme: 'file',
+		fileSystemPath: homedir()
+	});
+	return Uri.join(homedirUri, '.sfdx', 'alias.json');
 }
