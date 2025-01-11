2b72690c2bb8b33dea2e15d493385cec7277bc88
Cody T. Zeitler <codyzeitler12@gmail.com>
2025-01-11 11:26:56 -0700

    feat: change zoql script tree icon

--- src/vscode.ts
+++ src/vscode.ts
@@ -629,8 +629,8 @@ export class VscodeZoqlScriptTreeNode extends vscode.TreeItem {
     }
 
     iconPath = {
-        light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-        dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+        light: path.join(__filename, '..', '..', 'resources', 'db.svg'),
+        dark: path.join(__filename, '..', '..', 'resources', 'db.svg')
     };
 }
 
